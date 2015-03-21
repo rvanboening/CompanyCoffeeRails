9ad32ced9795a7a372df836229f9cd5a2c53e8e0
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-21 16:28:05 -0500

    Added User Model and set up user and drink table

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,3 +1,6 @@
 class Drink < ActiveRecord::Base
-  
+  attr_accessible :drink_name, :size, :reg_decaf, 
+                  :cream, :sugar, :reg_skim, :whip_nowhip, 
+                  :flavor, :wet_dry, :order_made, :order_date
+                  
 end
